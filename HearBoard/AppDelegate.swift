f0e853cce833ae834d0ee80da2e3b4dcfd1f66c1
dcweinstein <davidw1@iastate.edu>
2016-10-05 21:00:47 -0500

    Initial Commit

---
+++ HearBoard/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HearBoard
+//
+//  Created by David Weinstein on 10/5/16.
+//  Copyright © 2016 David Weinstein. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
